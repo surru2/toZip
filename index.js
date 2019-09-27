9c929477a7f32400c156b13c52bfac5d7a1e6870
Панов Владимир Александрович <panov-va@ur.rt.ru>
2019-09-27 14:47:46 +0500

    2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const getLinks = async () => { //получаем массив ссылок на изображения с https://yandex.ru/images/
-    const body = await fetch(`https://yandex.ru/images/`)
+    const body = await fetch(`https://yandex.ru/images/`).then(x=>x.text())
     const dom = new JSDOM(body);
     return Array.from(dom.window.document.getElementsByTagName('img')) //фильтруем возвращаемые url изображений только с абсолюными путями
         .map(t => t.src)
@@ -18,8 +18,7 @@ const toZip = (links) =>{
         fetch(img)
             .then(x => x.arrayBuffer())     //Преобразуем ответ в буффер и далее добавляем данные в архив
             .then(x => {
-                let zip = new AdmZip('files.zip');                 //во время каждой итерации открываем файл (хотя можно и не переназначить и использовать старый zip-объект
-                zip.addFile(i + '.jpg', Buffer.from(x)); //на потребление памяти это не повлияет) добавляем новые данные и сразу пишем его
+                zip.addFile(i + '.jpg', Buffer.from(x)); //во время каждой итерации открываем файл добавляем новые данные и сразу пишем его
                 zip.writeZip('files.zip');            //чтобы не держать в памяти архив со всеми изображениями
             });
     })

15bee36e40b61353e8b25da547dad7f8c76ec151
surru2 <surru@mail.ru>
2019-10-02 21:00:56 +0500

    pipe

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch');
 const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const {JSDOM} = jsdom;
 const archiver = require('archiver');
 const fs = require('fs');
+const { PassThrough } = require('stream')
 
 const getLinks = async () => {
     const body = await fetch(`https://yandex.ru/images/`).then(x => x.text())
@@ -13,25 +14,24 @@ const getLinks = async () => {
 }
 
 const toZip = (links) => {
-    const output = fs.createWriteStream(__dirname + '/files.zip');
+    const output = fs.createWriteStream(__dirname + '/files.zip')
+        .on('close',()=>console.log('Done'))
     const archive = archiver('zip');
     archive.pipe(output)
     links.reduce((prom, link) => {
         return prom.then(() => {
             return new Promise(resolve => {
                 fetch(link)
-                    .then(x => x.arrayBuffer())
                     .then(x => {
-                        console.log(`Getted ${link}`)
-                        archive
-                            .append(Buffer.from(x), { name : `${Math.random()}.jpg`.replace('.','') })
+                        const body = x.body.pipe(new PassThrough()
+                            .on('end',()=>console.log(`link ${link}`)))
+                        archive.append(body,{name: `${Math.random()}.jpg`.replace('.', '')})
                         resolve()
-                    });
+                    })
             });
         });
     }, Promise.resolve()).then(() => {
-            archive.finalize()
-            console.log('Done')
+        archive.finalize()
     });
 }
 
@@ -39,7 +39,7 @@ const myTest = async () => {
     let links = [];
     for (let i = 0; i < 4; i++) {
         await getLinks()
-            .then(arr => links=[...links,...arr]);
+            .then(arr => links = [...links, ...arr]);
     }
     console.log(`Fetched ${links.length} images from https://yandex.ru/images/`)
     toZip(links)
